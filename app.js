1576290ff7adc257de5671da779f5417afc141b9
saemjeon <38971167+saemjeon@users.noreply.github.com>
2021-06-03 16:47:40 -0400

    Some logging

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ module.exports = (app) => {
 
   app.on("pull_request.edited", async (context) => {
     app.log("Pull request edited");
+    app.log(context.octokit.pulls);
     return context.octokit.issues.createComment(
       context.issue({ body: "Pull request edited" })
     );
@@ -19,6 +20,7 @@ module.exports = (app) => {
 
   app.on("push", async (context) => {
     app.log("Some change is pushed");
+    app.log(context.octokit.pulls);
     return context.octokit.issues.createComment(
       context.issue({ body: "Some change is pushed" })
     );
